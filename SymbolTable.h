06934fc0a5d5236605f87ee88db6bfcf7e795808
Alexandrescu Andrei-Robert <ar_alexandrescu@yahoo.com>
2021-10-12 14:14:33 +0300

    Added tests to specification file

--- SymbolTable.h
+++ SymbolTable.h
@@ -26,4 +26,46 @@ public:
 	std::vector<std::string> getAllElements();
 	void print();
 };
+/*
+// SymbolTable tests
+SymbolTable* st = new SymbolTable(4);
 
+// insert
+std::pair<int, int> p(1, 0);
+assert(st->insert("a") == p);
+p = std::pair<int, int>(2, 0);
+assert(st->insert("b") == p);
+p = std::pair<int, int>(0, 0);
+assert(st->insert("T") == p);
+p = std::pair<int, int>(3, 0);
+assert(st->insert("c") == p);
+p = std::pair<int, int>(-1, -1); // insert duplicate
+assert(st->insert("c") == p);
+st->print();
+assert(st->exists("a") == true);
+assert(st->exists("b") == true);
+assert(st->exists("T") == true);
+assert(st->exists("c") == true);
+assert(st->exists("d") == false);
+assert(st->getAllElements().size() == 4);
+
+// resize operation performed here
+st->insert("d");
+st->print();
+assert(st->exists("a") == true);
+assert(st->exists("b") == true);
+assert(st->exists("T") == true);
+assert(st->exists("c") == true);
+assert(st->exists("d") == true);
+assert(st->getAllElements().size() == 5);
+
+// getPosition
+std::pair<int, int> p10(1, 0);
+assert(st->getPosition("a") == p10);
+std::pair<int, int> p41(4, 1);
+assert(st->getPosition("d") == p41);
+
+// hash
+int r = ((int)'a' + (int)'b' + (int)'c') % st->getCapacity();
+assert(st->hash("abc") == r);
+*/
